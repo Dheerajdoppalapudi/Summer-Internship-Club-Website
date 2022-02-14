a2c24e03325738f1976e4a28f784664a85d26918
Dheerajdoppalapudi <dheerudoppalapudi@gmail.com>
2022-02-14 22:41:44 +0530

    default route

--- sicwebapp/page/urls.py
+++ sicwebapp/page/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('internships/', views.internships, name='page-internships'),
     path('sug/', views.feedback, name='page-suggestions'),
-    path('about/', views.about, name='page-about'),
+    path('', views.about, name='page-about'),
     path('contactus/', views.contact_us, name='contact-us'),
     path('scholarships/', views.scholarships, name='page-scholarships'),
     path('certifications/', views.certifications, name='page-certifications'),
